7d3593868149145c52b8dfea3e47ce27b1c817d6
Prabhjot Singh <lazycoder001@gmail.com>
2026-05-10 17:25:19 +0530

    running sum solution optimized by removing extra variable

--- RunningSum.java
+++ RunningSum.java
@@ -1,25 +1,24 @@
 public class RunningSum {
-    public static int[] getRunningSum(int[] nums){
+    public static int[] getRunningSum(int[] nums) {
         int sum = 0;
         int[] result = new int[nums.length];
-        for(int i=0; i<nums.length; i++){
+        for (int i = 0; i < nums.length; i++) {
             sum += nums[i];
             result[i] = sum;
         }
         return result;
     }
 
-    public static int[] getRunningSumSpaceOptimized(int[] nums){
-        int sum = nums[0];
-        for(int i=1; i<nums.length; i++){
-            sum += nums[i];
-            nums[i] = sum;
+    public static int[] getRunningSumSpaceOptimized(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {
+            nums[i] = nums[i - 1] + nums[i];
         }
         return nums;
     }
+
     public static void main(String[] args) {
-        int[] nums = {1,2,3,4};
-        for(int i : getRunningSumSpaceOptimized(nums)){
+        int[] nums = { 1, 2, 3, 4 };
+        for (int i : getRunningSumSpaceOptimized(nums)) {
             System.out.println(i);
         }
     }
